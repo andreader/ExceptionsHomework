ca0752405ef6c94802ec0b4c2aba7887eeee816d
Андрей <79538233369@ya.ru>
2024-02-27 11:46:58 +0500

    Initial

---
+++ src/main/java/sky/pro/ExceptionsHomework/ExceptionsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.ExceptionsHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/ExceptionsHomework/ExceptionsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.ExceptionsHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
